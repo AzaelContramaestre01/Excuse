034b4ce5d42282d9117705a1d4a0ef6f17fb56d0
Azael Contramaestre <72152743+AzaelContramaestre01@users.noreply.github.com>
2020-11-20 20:36:03 -0300

    Fix.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-window.onload = function(){
-   
-    var adj = ['Un mago ', 'Un desarrollador ', 'Alguien enojado ', 'El vicioso ','El millonario ',];
-    var pronombre = ['trotador ', 'estudioso ','perruno ', 'mercenario ', 'filantropo ', 'comediante ',];
-    var accion = ['expropio mi ', 'le gusto mi ', 'quemo mi ' , 'atrapo mi ', 'encanto mi ', 'le pego a mi ',];
-    var posesion = ['novia ', 'perro ', 'flecha ', 'pc  ', 'licencia ', 'dedo indice ',];
-    var lugar = ['en mi casa','en tu casa','en nuestra casa', 'en la casa de ellos', 'en casa de hercules', 'en casa de eritrea',];
-    
+window.onload = function () {
 
-     var n1 = Math.floor((Math.random() * adj.length) );
-     var n2 = Math.floor((Math.random() * pronombre.length) );
-     var n3 = Math.floor((Math.random() * accion.length) );
-     var n4 = Math.floor((Math.random() * posesion.length) );
-     var n5 = Math.floor((Math.random() * lugar.length) );
-    
-      
-      document.querySelector('#excuse').innerHTML = excuse; 
-    
-  }
+    let adj = ['Un mago ', 'Un desarrollador ', 'Alguien enojado ', 'El vicioso ', 'El millonario ',];
+    let pronombre = ['trotador ', 'estudioso ', 'perruno ', 'mercenario ', 'filantropo ', 'comediante ',];
+    let accion = ['expropio mi ', 'le gusto mi ', 'quemo mi ', 'atrapo mi ', 'encanto mi ', 'le pego a mi ',];
+    let posesion = ['novia ', 'perro ', 'flecha ', 'pc  ', 'licencia ', 'dedo indice ',];
+    let lugar = ['en mi casa', 'en tu casa', 'en nuestra casa', 'en su casa', 'en casa de hercules', 'en casa de eritrea',];
+
+
+    let n1 = Math.floor((Math.random() * adj.length));
+    let n2 = Math.floor((Math.random() * pronombre.length));
+    let n3 = Math.floor((Math.random() * accion.length));
+    let n4 = Math.floor((Math.random() * posesion.length));
+    let n5 = Math.floor((Math.random() * lugar.length));
+
+
+    document.querySelector('#excuse').innerHTML = adj[n1] + pronombre[n2] + accion[n3] + posesion[n4] + lugar[n5];
+
+}
